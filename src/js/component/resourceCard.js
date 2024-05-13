a8d28040ac15217b43eed7fe54829f9a12be190e
MaríaSF <158471273+DexpinBe@users.noreply.github.com>
2024-05-13 10:50:16 +0000

    Vista Home terminada

--- src/js/component/resourceCard.js
+++ src/js/component/resourceCard.js
@@ -1,8 +1,6 @@
-
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import { Context } from "../store/appContext.js";
 import { Link } from "react-router-dom";
-
 import "../../styles/home.css";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -11,31 +9,29 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { Container } from 'react-bootstrap';
 
 function ResourceCard(props) {
-    console.log("Props en ResourceCard:", props);
     const { store, actions } = useContext(Context);
 
+    const resourceImageUrls = {
+        people: (uid) => `https://starwars-visualguide.com/assets/img/characters/${uid}.jpg`,
+        species: (uid) => `https://starwars-visualguide.com/assets/img/species/${uid}.jpg`,
+        planets: (uid) => `https://starwars-visualguide.com/assets/img/planets/${uid}.jpg`
+    };
+
+    const getImageUrl = resourceImageUrls[props.resource] || ((uid) => `https://starwars-visualguide.com/assets/img/${props.resource}/${uid}.jpg`);
+    const imageUrl = getImageUrl(props.uid);
 
     return (
-        <Card className="card" style={ {width: '18rem'} } key={props.id}>
-            {(props.resource === 'people')
-            ? <Card.Img variant="top" src={`https://starwars-visualguide.com/assets/img/characters/${props.uid}.jpg`} />
-            : (props.resource === 'species')
-             ? <Card.Img variant="top" src={`https://starwars-visualguide.com/assets/img/species/${props.uid}.jpg`} />
-             : (props.resource === 'planets')
-               ? <Card.Img variant="top" src={`https://starwars-visualguide.com/assets/img/planets/${props.uid}.jpg`} />
-               : <Card.Img variant="top" src={`https://starwars-visualguide.com/assets/img/${props.resource}/${props.uid}.jpg`} />
-      
-        }
-        <Card.Body>
-            <Card.Title>{props.name}</Card.Title>
-            <div className="d-flex justify-content-between">
-                <Link className="btn btn-primary" to={"/" + props.resource + "/" + props.uid} resource={props.resource}>Read More</Link>
-                <Button variant="warning" onClick={(e)=>actions.addFavorite(props.name, props.uid, props.resource)}><FontAwesomeIcon icon={faHeart} /></Button>
-            </div>
-        </Card.Body>
+        <Card className="card" style={{ width: '18rem', marginBottom: '20px' }} key={props.id}>
+            <Card.Img variant="top" src={imageUrl} />
+            <Card.Body>
+                <Card.Title>{props.name}</Card.Title>
+                <div className="d-flex justify-content-between">
+                    <Link className="btn btn-primary" to={`/${props.resource}/${props.uid}`} resource={props.resource}>Read More</Link>
+                    <Button variant="warning" onClick={(e) => actions.addFavorite(props.name, props.uid, props.resource)}><FontAwesomeIcon icon={faHeart} /></Button>
+                </div>
+            </Card.Body>
         </Card>
-     )
-     
+    );
 }
 
-export default ResourceCard
+export default ResourceCard;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -8,54 +8,71 @@ import Col from 'react-bootstrap/Col';
 import ResourceCard from "../component/resourceCard.js";
 
 
+
 export const Home = () => {
 
-	const { store, actions } = useContext(Context);
+    const { store, actions } = useContext(Context);
 
-	useEffect(()=>{
-		actions.getSWAPI('people');
-		actions.getSWAPI('species');
-		actions.getSWAPI('planets')
-	}, []);
+    useEffect(() => {
+        actions.getSWAPI('people');
+        actions.getSWAPI('species');
+        actions.getSWAPI('planets')
+    }, []);
 
-	return (
-		<Container className="mt-5">
-			<Row>
-			<Col>
-		 			<h1 className="text-danger">Characters</h1>
-		 			<div className="scroll-horizontal">
-		 				{store.people.map((character) => {
-		 					return (
-		 						<ResourceCard key={character.uid} uid={character.uid} name={character.name} resource="people"/>
-		 					)
-		 				})}
-		 			</div>
-		 		</Col>
-			</Row>
-			<Row className="mt-4">
-				<Col>
-				<h1 className="text-danger">Species</h1>
-				<div className="scroll-horizontal">
-		 				{store.species.map((specie) => {
-		 					return (
-		 						<ResourceCard key={specie.uid} uid={specie.uid} name={specie.name} resource="species"/>
-		 					)
-		 				})}
-		 			</div>
-					 </Col>
-			</Row>
-			<Row className="mt-4">
-				<Col>
-				<h1 className="text-danger">Planets</h1>
-				<div className="scroll-horizontal">
-		 				{store.planets.map((planet) => {
-		 					return (
-		 						<ResourceCard key={planet.uid} uid={planet.uid} name={planet.name} resource="planets" />
-		 					)
-		 				})}
-		 			</div>
-					 </Col>
-			</Row>
-		</Container>
-	)
+    return (
+        <Container className="mt-5">
+            <Row className="mt-2">
+                <Col xs={12}>
+                    <h1 className="text-danger">Characters</h1>
+                </Col>
+            </Row>
+            <Row className="flex-nowrap overflow-auto">
+                <Col xs={12} className="d-flex flex-nowrap">
+                    <div className="scroll-horizontal d-flex flex-nowrap">
+                        {store.people.map((character) => {
+                            return (
+                                <ResourceCard key={character.uid} uid={character.uid} name={character.name} resource="people" />
+                            )
+                        })}
+                    </div>
+                </Col>
+            </Row>
+            <Row className="mt-2">
+                <Col xs={12}>
+                    <h1 className="text-danger">Species</h1>
+                </Col>
+            </Row>
+            <Row className="mt-4 flex-nowrap overflow-auto">
+                <Col xs={12} className="d-flex flex-nowrap">
+
+                    <div className="scroll-horizontal d-flex flex-nowrap">
+                        {store.species.map((specie) => {
+                            return (
+                                <ResourceCard key={specie.uid} uid={specie.uid} name={specie.name} resource="species" />
+                            )
+                        })}
+                    </div>
+                </Col>
+            </Row>
+            <Row className="mt-2">
+                <Col xs={12}>
+                    <h1 className="text-danger">Planets</h1>
+                </Col>
+            </Row>
+            <Row className="mt-4 flex-nowrap overflow-auto">
+                <Col xs={12} className="d-flex flex-nowrap">
+
+                    <div className="scroll-horizontal d-flex flex-nowrap">
+                        {store.planets.map((planet) => {
+                            return (
+                                <ResourceCard key={planet.uid} uid={planet.uid} name={planet.name} resource="planets" />
+                            )
+                        })}
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+
+
+    )
 }
